94a6ff7f155ddf6f425ad1e762ea93ddbccc9474
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-08-25 16:36:45 +0200

    add header page structure, styles and animation. Add burguer menu on mobile version in order to be responsive

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,4 +1,13 @@
 'use strict';
-console.log('ready');
+const burguerMenu = document.querySelector('.burguer-menu');
+const headerNav = document.querySelector('.header__nav');
+
+
+
+function handlerBurguerMenuClick() {
+  headerNav.classList.toggle('header__nav-fade-in');
+}
+
+burguerMenu.addEventListener('click', handlerBurguerMenuClick);
 
 
