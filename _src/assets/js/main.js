57bb6e08723093fccef7df489d9839993b0fa737
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-08-26 14:34:27 +0200

    add contact map from google.maps documentation

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -10,4 +10,23 @@ function handlerBurguerMenuClick() {
 
 burguerMenu.addEventListener('click', handlerBurguerMenuClick);
 
+//Google Maps funcionality
 
+// Initialize and add the map
+function initMap() {
+  // The location
+  var englishHomelandCoords = {lat: 42.878876, lng: -8.539355};
+  // The map, centered at location
+  var map = new google.maps.Map(
+    document.querySelector('.gmaps_frame__map'),
+    {
+      zoom: 17,
+      center: englishHomelandCoords
+    }
+  );
+  // The marker, positioned at location
+  var marker = new google.maps.Marker({
+    position: englishHomelandCoords,
+    map: map
+  });
+}
